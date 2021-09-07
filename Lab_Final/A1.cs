f55771cd7a4146c45b9c57febde8def8264dc79e
pondjs <ponejirasuk@mgail.com>
2021-09-08 00:00:51 +0700

    Add project files.

---
+++ Lab_Final/A1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_Final
+{
+    class A1
+    {
+        public void Run()
+        {
+            string input;
+            bool isExit = false;
+            while(!isExit)
+            {
+                Console.Write("menu :> ");
+                input = Console.ReadLine();
+
+                if(input == "4")
+                {
+                    isExit = true;
+                }
+                
+            }
+        }
+
+        void MenuString()
+        {
+            Console.WriteLine("1 - Line");
+            Console.WriteLine("2 - Rectangle");
+            Console.WriteLine("3 - Triangle");
+            Console.WriteLine("4 - Exit");
+            Console.WriteLine("------------");
+        }
+    }
+}
---
+++ Lab_Final/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lab_Final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            A1 a1 = new A1();
+            a1.Run();
+        }
+    }
+}
