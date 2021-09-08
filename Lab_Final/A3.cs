4fb96bc501f4d73e4514fcbd201af3dd8b4609bd
pondjs <ponejirasuk@mgail.com>
2021-09-08 11:44:20 +0700

    A3

---
+++ Lab_Final/A3.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_Final
+{
+    class A3
+    {
+        public void Run()
+        {
+            Console.Write("how many number :> ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            double[] data = new double[input];
+            for (int i = 0; i < data.Length; i++)
+            {
+                try
+                {
+                    Console.Write($"input data[{i + 1}] :> ");
+                    data[i] = Convert.ToDouble(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("***invalid value***");
+                    i--;
+                }
+            }
+            double[] maxMin = MaxMin(data);
+            Console.WriteLine($"\nMax is {maxMin[0]}");
+            Console.WriteLine($"Min is {maxMin[1]}");
+            Console.WriteLine($"Diff is {Math.Abs(maxMin[0] - maxMin[1])}");
+        }
+        private double[] MaxMin(double[] data)
+        {
+            double[] maxMin = { data[0], data[0] };
+            foreach (double value in data)
+            {
+                if (value > maxMin[0])
+                {
+                    maxMin[0] = value;
+                }
+                if (value < maxMin[1])
+                {
+                    maxMin[1] = value;
+                }
+            }
+            return maxMin;
+        }
+    }
+}
--- Lab_Final/Program.cs
+++ Lab_Final/Program.cs
@@ -10,8 +10,20 @@ namespace Lab_Final
             //A1 a1 = new A1();
             //a1.Run();
 
-            A2 a2 = new A2();
-            a2.Run();
+            //A2 a2 = new A2();
+            //a2.Run();
+
+            A3 a3 = new A3();
+            a3.Run();
+
+            //A4 a4 = new A4();
+            //a4.Run();
+
+            //A5 a5 = new A5();
+            //a5.Run();
+
+            //A6 a6 = new A6();
+            //a6.Run();
 
         }
     }
