660aabbd1ca6b6c2c69abb064c2b14d1e0496957
pondjs <ponejirasuk@mgail.com>
2021-09-08 14:15:55 +0700

    comment A1

--- Lab_Final/Program.cs
+++ Lab_Final/Program.cs
@@ -7,8 +7,8 @@ namespace Lab_Final
         static void Main(string[] args)
         {
             
-            //A1 a1 = new A1();
-            //a1.Run();
+            A1 a1 = new A1();
+            a1.Run();
 
             //A2 a2 = new A2();
             //a2.Run();
@@ -22,8 +22,8 @@ namespace Lab_Final
             //A5 a5 = new A5();
             //a5.Run();
 
-            A6 a6 = new A6();
-            a6.Run();
+            //A6 a6 = new A6();
+            //a6.Run();
 
         }
     }
